2352aad5857d41c64157c3ff29d9d6b19a9a0b41
fu0v <ayoubelouardioff@gmail.com>
2023-10-06 00:36:53 +0100

    h

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,13 @@
-#include<unistd.h>
+nclude <stdio.h>
+#include <unistd.h>
 
 /**
-* main - Entry point
-*
-* Return: Always 0 (Success)
-*/
+ * main - prints exactly
+ * followed by a new line, to the standard error.
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	char q[54] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-	write(1.q.59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
