ab11310155c10ea77f5469f08611835a035f8690
fu0v <ayoubelouardioff@gmail.com>
2023-10-05 23:59:05 +0100

    h

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,18 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /**
-* main - Entry point
-*
-* Return: Always 0 (Success)
-*/
+ * main - entry point
+ *
+ * description: ptiny the size of various var types
+ *
+ * Return: 0 if (Success)
+ */
 int main(void)
 {
-	pirntf("Size of char: %d byte(s)\n", sizeof(char));
-	pirntf("Size of an int: %d byte(s)\n", sizeof(int));
-	pirntf("Size of a long int: %d byte(s)\n", sizeof(long int));
-	pirntf("Size of a long long int: %d byte(s)\n", (long long int));
-	pirntf("Size of a float: %d byte(s)\n", sizeof(float));
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
 	return (0);
 }
