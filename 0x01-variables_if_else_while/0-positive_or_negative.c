a20bb04a43f44aec6a2b4df90e1a665f4dcc1e90
fu0v <ayoubelouardioff@gmail.com>
2023-10-07 01:04:22 +0100

    f3

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,15 +17,15 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
-		printf("is positive\n");
+		printf("is positive");
 	}
 	else if (n == 0)
 	{
-		printf("is zero\n");
+		printf("is zero");
 	}
 	else
 	{
-		printf("is negative\n");
+		printf("is negative");
 	}
 	return (0);
 }
