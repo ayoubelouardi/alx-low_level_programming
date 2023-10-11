592df52db8af15f00b00a5175d0e1a0064db18d9
fu0v <ayoubelouardioff@gmail.com>
2023-10-12 00:21:50 +0100

    done

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+void print_alphabet(void)
+{
+	int x;
+	for (x = 98; x <= 124; x++)
+	{
+		_putchar(x);
+	}
+	_putchar('\n');
+	return (0);
+}
