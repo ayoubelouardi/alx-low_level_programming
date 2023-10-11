3529deeed9af2dad405be2f9b761e866ffe91333
fu0v <ayoubelouardioff@gmail.com>
2023-10-12 00:43:27 +0100

    done

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* print_alphabet_x10 - Entry point
+* Return: Always 0 (Success)
+*/
+void print_alphabet_x10(void)
+{
+	int x, i;
+	for (i = 1; i <= 10; i++)
+	{
+		for (x = 97; x <= 122; x++)
+		{
+			_putchar(x);
+		}
+		_putchar('\n');
+	}
+}
