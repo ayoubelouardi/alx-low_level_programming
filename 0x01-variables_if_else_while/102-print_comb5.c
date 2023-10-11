3bb3b69f8f97481242b433b77085bce3e224b34e
fu0v <ayoubelouardioff@gmail.com>
2023-10-12 00:55:15 +0100

    done

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int i, j, k, l;
+
+	for (i = 48; i <= 57; i++)
+	{
+		for (j = 48; j <= 57; j++)
+		{
+			if (i < j)
+			{
+				putchar(i);
+				putchar(j);
+				if (!(i == 56 && j == 57))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
