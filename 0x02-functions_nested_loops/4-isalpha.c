1658bc8bb8cd13bceaa9e422b33187fe069e1089
fu0v <ayoubelouardioff@gmail.com>
2023-10-12 01:15:22 +0100

    done

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* _isalpha - Entry point
+* @c: the charachter 
+* Return: 1 if c is alpha or 0 elseway
+*/
+int _isalpha(int c)
+{
+	if (c > 96 && c < 123)
+	{
+		return (1);
+	}
+	else if (c > 64 && c < 91)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
