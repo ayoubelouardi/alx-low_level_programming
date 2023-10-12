b143afc0d0a9239308f3be698e6a9b6b13ef50ec
fu0v <ayoubelouardioff@gmail.com>
2023-10-12 01:38:46 +0100

    done

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ ** abs - prints abs value of an int
+ ** @n: the number
+ ** Return: return the abs of the number
+ **/
+int abs(int n)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (n * -);
+	}
+}
