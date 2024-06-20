96e444e5a922128ac80255b8430477c76ab027de
Ayoub El Ouardi <ayoubelouardioff@gmail.com>
2024-06-20 22:50:10 +0100

    fixing error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\0";
+	char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 
 	write(2, &msg, sizeof(msg));
 	return (0);
