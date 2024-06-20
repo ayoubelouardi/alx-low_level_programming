8dbf8ca3c5ebbeaf7bcb04da6dc2f95ab9188201
Ayoub El Ouardi <ayoubelouardioff@gmail.com>
2024-06-20 23:35:53 +0100

    advancing

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,13 +2,16 @@
 
 /**
  * main - print text with write
+ *
  * Description: using write to print the text
- * Return: 0 if success
+ *
+ * Return: return error 1
  */
+
 int main(void)
 {
-	char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(2, &msg, sizeof(msg));
-	return (0);
+	write(1, msg, 59);
+	return (1);
 }
