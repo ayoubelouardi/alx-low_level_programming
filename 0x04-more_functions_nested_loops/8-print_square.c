5114d34c2e5e8f79d85d8ced47174d6e84024c77
Ayoub El Ouardi <ayoubelouardioff@gmail.com>
2024-06-28 02:03:59 +0100

    fixing

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -3,17 +3,18 @@
 /**
  * print_square - print a square using the character #
  * @size: is the size of the square
- * Return: Always 0 (Success)
 */
 
 void print_square(int size)
 {
 	int i, j;
 
-	for (i = 1; i <= size; ++i)
+	for (i = 0; i < size; ++i)
 	{
-		for (j = 1; j <= size; ++j)
+		for (j = 0; j < size; ++j)
+		{
 			_putchar('#');
+		}
 		_putchar('\n');
 	}
 }
