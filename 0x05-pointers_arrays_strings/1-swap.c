8c3d956de6406c78896c5dc7b009185beebe70a5
Ayoub El Ouardi <ayoubelouardioff@gmail.com>
2024-07-02 01:26:52 +0100

    completed

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+/**
+ * swap_int - swaps the values of two integers
+ * using two input parameters
+ * @a: input parameter 1
+ * @b: input parameter 2
+*/
+
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
