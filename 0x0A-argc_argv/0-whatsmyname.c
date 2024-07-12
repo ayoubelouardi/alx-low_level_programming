25e268c894cf67ddef7f69586b22d7e621bbe4c8
Ayoub El Ouardi <ayoubelouardioff@gmail.com>
2024-07-12 21:31:51 +0100

    completed

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - print my name
+ * @argc: n of arg
+ * @argv: array of arg
+ * Return: 0 if success
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
