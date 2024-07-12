63bdf878cf4604f45ee35e957c01dd4d8bcb8831
Ayoub El Ouardi <ayoubelouardioff@gmail.com>
2024-07-12 22:29:19 +0100

    completed

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - adder
+ * @argc: args n
+ * @argb: args values
+ * Return: 0 if success, 1 otherwise
+ */
+
+int main(int argc, char *argv[])
+{
+	int i = 1;
+	long int r = 0;
+	char *ptr = '\0';
+
+	while (i < argc)
+	{
+		r += strtol(argv[i], &ptr, 10);
+		if (*ptr != '\0')
+		{
+			puts("Error");
+			return (1);
+		}
+		i++;
+	}
+	printf("%ld\n", r);
+
+	return (0);
+}
