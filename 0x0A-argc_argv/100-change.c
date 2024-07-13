3d9ee0522f63570d35c7eb3d43fd44b3d298402a
Ayoub El Ouardi <ayoubelouardioff@gmail.com>
2024-07-13 13:12:29 +0100

    completed

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -10,20 +10,19 @@
 
 int main(int argc, char *argv[])
 {
-	int cents, i = 0;
+	unsigned int cents, i = 0;
 
 	if (argc != 2)
 	{
 		puts("Error");
 		return (1);
 	}
-
-	cents = atoi(argv[1]);
-	if (cents < 0)
+	if (atoi(argv[1]) < 0)
 	{
 		printf("%d\n", 0);
 		return (0);
 	}
+	cents = atoi(argv[1]);
 	while (cents)
 	{
 		if (cents % 25 == 0)
