a7fccb27c7f4080277679568260aeb515211946a
Ayoub El Ouardi <ayoubelouardioff@gmail.com>
2024-07-15 18:52:08 +0100

    wellllcome memory bytes!

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * create_array - creates an array of chars,
+ * and initializes it with a specific char.
+ * @size: Description of parameter x
+ * @c: Description of parameter x
+ * Return:
+ * NULL if size = 0
+ * pointer to the array otherwise
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *ptr;
+	size_t i;
+
+	ptr = (char *)malloc(size * char);
+	if (size == 0 || ptr == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		ptr[i] = c;
+
+	return (ptr);
+}
