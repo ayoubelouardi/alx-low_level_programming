4e0be1a4bcde200621f8f7deea8b7f8b93a0359c
Ayoub El Ouardi <ayoubelouardioff@gmail.com>
2024-07-16 18:28:32 +0100

    completed

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -21,7 +21,7 @@ char *str_concat(char *s1, char *s2)
 
 	/* if s1 / s2 is NULL return NULL */
 	if (s1 == NULL || s2 != NULL)
-		return (s1);
+		return (NULL);
 
 	/* calcul the size of s1 and s2 */
 	while (s1[i++] != '\0')
