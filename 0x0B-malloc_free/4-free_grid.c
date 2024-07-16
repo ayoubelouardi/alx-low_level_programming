09ede919096000b606d0cc7d1f21ee480d67748e
Ayoub El Ouardi <ayoubelouardioff@gmail.com>
2024-07-16 21:29:00 +0100

    completed

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -12,8 +12,8 @@ void free_grid(int **grid, int height)
 {
 	if (grid != NULL && height == 0)
 	{
-		while (height >= 0)
-			free(grid[height--]);
+		while (height > 0)
+			free(grid[--height]);
 		free(grid);
 	}
 }
