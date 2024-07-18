271d159600dea2797dc6bf907eeb44b62ec6c090
Ayoub El Ouardi <ayoubelouardioff@gmail.com>
2024-07-18 23:02:08 +0100

    completed

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,42 +1,46 @@
 #include "main.h"
-#include "0-malloc_checked.c"
 
 /**
- * _calloc - allocates memory for an array, using malloc.
+ * *_memset - fills memory with a constant byte
+ * @s: memory area to be filled
+ * @b: char to copy
+ * @n: number of times to copy b
+ * Return: pointer to the memory area s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+
+	return (s);
+}
+
+/**
+ * *_calloc - allocates memory for an array
+ * @nmemb: number of elements in the array
+ * @size: size of each element
  *
- * @nmemb: number of array members
- * @size: size of each member with bytes
- *
- * Description:
- *
- * he _calloc function allocates memory for an array of nmemb
- * elements of size bytes each and returns a pointer
- * to the allocated memory.
- *
- * The memory is set to zero
- *
- * Return:
- * If nmemb or size is 0, then _calloc returns NULL
- * If malloc fails, then _calloc returns NULL
- * otherwise return void pointer to the adress of memory
- *
+ * Return: pointer to allocated memory
  */
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *ptr;
-	size_t i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	ptr = (char *) malloc_checked(nmemb * size);
+	ptr = malloc(size * nmemb);
 
 	if (ptr == NULL)
-		return (NULL):
+		return (NULL);
 
-	for (i = 0; i < (size_t) size * nmemb; i++)
-		ptr[i] = 0;
+	_memset(ptr, 0, nmemb * size);
 
 	return (ptr);
 }
