6571f33fcd1d56d8f99d92009e11a6433d37084d
Ayoub El Ouardi <ayoubelouardioff@gmail.com>
2024-07-19 20:10:38 +0100

    completed

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,47 +1,38 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+
 /**
- * main - print the value of n
- * Description: print if the number is greater than 0: is positive
- * if the number is 0: is zero
- * if the number is less than 0: is negative
+ * main - Entry point
+ * @parameterx: Description of parameter x
+ * Description: Longer description of the function)?
  * Return: 0 if success
  */
+
 int main(void)
 {
-	int a = 0;
-	int b, c;
+	int x, y, z;
 
-	while (a <= 9)
+	for (x = 0; x <= 7; x++)
 	{
-		b = 0;
-		while (b <= 9)
+		for (y = 1; y <= 8; y++)
 		{
-			c = 0;
-			while (c <= 9)
+			for (z = 2; z <= 9; z++)
 			{
-				if (a != b &&
-				    a < b &&
-				    b != c &&
-				    b < c)
+				if (x >= y || y >= z)
 				{
-					putchar(a + 48);
-					putchar(b + 48);
-					putchar(c + 48);
-
-					if (a + b + c != 24)
-					{
-						putchar(',');
-						putchar(' ');
-					}
+					continue;
 				}
-				++c;
+				else
+				{
+					printf("%d%d%d", x, y, z);
+					if (x == 7 && y == 8 && z == 9)
+						continue;
+					else
+						printf(", ");
+				}
 			}
-			++b;
 		}
-		++a;
 	}
-	putchar('\n');
+	puts("");
+
 	return (0);
 }
