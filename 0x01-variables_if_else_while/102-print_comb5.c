51df6504d0961216a6309e45cbaa854b7159f883
Ayoub El Ouardi <ayoubelouardioff@gmail.com>
2024-07-19 20:47:56 +0100

    completed

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,40 +1,45 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+
 /**
- * main - print the value of n
- * Description: print if the number is greater than 0: is positive
- * if the number is 0: is zero
- * if the number is less than 0: is negative
+ * main - Entry point
+ * Description: Longer description of the function)?
  * Return: 0 if success
  */
+
 int main(void)
 {
-	int a = 0, b;
+	int x, y;
 
-	while (a <= 99)
+	for (x = 0; x <= 98; x++)
 	{
-		b = a;
-		while (b <= 99)
+		for (y = 1; y <= 99; y++)
 		{
-			if (b != a)
+			if (x >= y)
 			{
-				putchar((a / 10) + 48);
-				putchar((a % 10) + 48);
-				putchar(' ');
-				putchar((b / 10) + 48);
-				putchar((b % 10) + 48);
-
-				if (a != 98 || b != 99)
-				{
-					putchar(',');
-					putchar(' ');
-				}
+				continue;
 			}
-			++b;
+			else
+			{
+				if (x < 10)
+				{
+					printf("0%d", x);
+				}
+				printf("%d", x);
+				printf(" ");
+				if (y < 10)
+				{
+					printf("0%d", y);
+				}
+				printf("%d", y);
+				if (x == 8 && y == 9)
+					continue;
+				else
+					printf(", ");
+			}
 		}
-		++a;
 	}
-	putchar('\n');
+	puts("");
+
 	return (0);
 }
+
