2733a1585f6ef151dffdb515524b6051748baf43
Ayoub El Ouardi <ayoubelouardioff@gmail.com>
2024-07-23 15:49:58 +0100

    completed

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,22 +1,16 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
  * main - print the name of the file
  * Description: prints the name of the file it was
  * compiled from, followed by a new line.
- * Return: 0 if success
+ * Return: 0 Always.
  */
 
 int main(void)
 {
-	char *s = __FILE__;
 
-	while (*s != '\0')
-	{
-		_putchar(*s);
-		s++;
-	}
-	_putchar('\n');
+	puts(__FILE__);
 
 	return (0);
 }
