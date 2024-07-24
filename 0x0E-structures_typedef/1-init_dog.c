3de0446aa41a8a32e6562505b3468e8b45e28df0
Ayoub El Ouardi <ayoubelouardioff@gmail.com>
2024-07-24 17:05:42 +0100

    completed

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,42 +1,4 @@
-#include <stdlib.h>
 #include "dog.h"
-
-/**
- * _strcpy - returns the length of a string
- * @src: string parameter input
- * @dest: string parameter input
- * Return: length of string
- */
-
-char *_strcpy(char *dest, char *src)
-{
-	int a, b = 0;
-
-	for (a = 0; src[a] != '\0'; ++a)
-	{
-		dest[b] = src[a];
-		++b;
-	}
-	dest[b] = '\0';
-
-	return (dest);
-}
-
-/**
- * _strlen - returns the length of a string
- * @s: string parameter input
- * Return: length of string
- */
-
-int _strlen(char *s)
-{
-	int counter;
-
-	for (counter = 0; *s != '\0'; ++s)
-		++counter;
-
-	return (counter);
-}
 
 /**
  * init_dog - initialize a variable of type struct dog
@@ -49,14 +11,10 @@ int _strlen(char *s)
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = malloc(_strlen(name) + 1);
-	d->owner = malloc(_strlen(owner) + 1);
-
-	if (d->name == NULL || d->owner == NULL)
-		exit(1);
-
-	d->name = _strcpy(d->name, name);
-	d->owner = _strcpy(d->owner, owner);
-
-	d->age = age;
+	if (d)
+	{
+		(*d).name = name;
+		(*d).age = age;
+		(*d).owner = owner;
+	}
 }
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,15 +9,24 @@
  * Description: Define a new type struct dog
  */
 
-typedef struct dog
+
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog_t;
+}; 
 
+/**
+ * typedef dog_t - type for dogs
+ */
+typedef struct dog dog_t;
+
+
+/* functions */
 void init_dog(struct dog *d, char *name, float age, char *owner);
-
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
