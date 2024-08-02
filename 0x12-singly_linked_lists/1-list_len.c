74b82f3cef372a77f9cb59bb49bf752f9efedd60
Ayoub El Ouardi <ayoubelouardioff@gmail.com>
2024-08-02 20:39:55 +0100

    completed

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -7,17 +7,18 @@
  * Description: function that returns the number of elements
  * in a linked list_t list.
  * Return: number of nodes
-*/
-size_t list_len(const list_t *h) 
+ */
+size_t list_len(const list_t *h)
 {
-        size_t node_count = 1;
+	size_t node_count = 1;
 
-        if (h == NULL)
-                return (0);
-        while (h->next != NULL)
-        {   
-                h = h->next;
-                node_count++;
-        }   
-        return (node_count);
+	if (h == NULL)
+		return (0);
+
+	while (h->next != NULL)
+	{
+		h = h->next;
+		node_count++;
+	}
+	return (node_count);
 }
