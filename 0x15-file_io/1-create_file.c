ccdd269928847dd4ff8965345ec8f0b01e51f4d8
Ayoub El Ouardi <ayoubelouardioff@gmail.com>
2024-09-11 00:57:59 +0100

    lets find out if this trick works

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -28,9 +28,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename,
-			O_WRONLY | O_CREAT | O_APPEND,
-			0600);
+	fd = creat(filename, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
 
