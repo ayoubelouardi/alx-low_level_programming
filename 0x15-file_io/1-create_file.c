36019ae59d56ac88221ef8e5647af8c57dc72b3a
Ayoub El Ouardi <ayoubelouardioff@gmail.com>
2024-09-11 00:25:05 +0100

    this is fine

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,18 +18,18 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd;	
+	int fd;
 	ssize_t size = 0;
 
 	if (text_content != NULL)
-		while (text_content[size]!= '\0')
+		while (text_content[size] != '\0')
 			size++;
 
 	if (filename == NULL)
 		return (-1);
 
 	fd = open(filename,
-			O_WRONLY | O_EXCL | O_CREAT | O_APPEND,
+			O_WRONLY | O_CREAT | O_APPEND,
 			0600);
 	if (fd == -1)
 		return (-1);
