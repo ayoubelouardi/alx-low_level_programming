803d6f6ab576f2bfa87b6b4b3c3d0230af3deaaa
Ayoub El Ouardi <ayoubelouardioff@gmail.com>
2024-09-19 22:22:04 +0100

    completed

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_dlistint - print a double linked list
+ * @h: dlistint_t pointer
+ * Description:
+ * function that prints all the elements of a dlistint_t list.
+ * Return: the number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h);
+{
+	size_t n = 0;
+
+	while (h->next != NULL)
+	{
+		print("%d\n", h->n);
+		n++;
+	}
+	return (n);
+}
