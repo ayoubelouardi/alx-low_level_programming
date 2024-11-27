672f1e8da6b7ecece8b40bd8e813d77002b3d2a4
Ayoub El Ouardi <ayoubelouardioff@gmail.com>
2024-11-28 00:49:54 +0100

    complete this project, no need for copy and past compiler commands anymore :)

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+function that returns the perimeter of
+an island described in grid
+"""
+
+
+def determine_soroundings(array, y, x):
+    """
+    Determines whether soroundings has water
+    and if it's next or previous is at the ends
+    Args:
+        array (list): grid
+        y (int): current list index in `array`
+        x (int): current index in the list in `array`
+    """
+    mask = 1
+    top = array[y - 1][x] ^ mask if y > 0 else 1
+    bottom = array[y + 1][x] ^ mask if y < (len(array) - 1) else 1
+    left = array[y][x - 1] ^ mask if x > 0 else 1
+    right = array[y][x + 1] ^ mask if x < (len(array[y]) - 1) else 1
+    positions = top + bottom + right + left
+    return positions
+
+
+def island_perimeter(grid):
+    """
+    Gets the perimeter determined.
+    Args:
+        grid (list): list of lists
+    """
+    land = 1
+    perimeter = 0
+    for i in range(len(grid)):
+        for j, x in enumerate(grid[i]):
+            if x == land:
+                perimeter += determine_soroundings(grid, i, j)
+    return perimeter
