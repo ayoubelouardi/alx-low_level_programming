5fce643f4bfe436b2ffae76b4682373ce45fb71c
Ayoub El Ouardi <ayoubelouardioff@gmail.com>
2025-01-17 20:32:10 +0100

    completed

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+#include <string.h>
+
+/**
+ * main - Write a C program that prints a specific quote to standard error without using printf or puts.
+ *
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	char *quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	size_t size = strlen(quote);
+
+	write(2, quote, size);
+
+	return (1);
+}
