04ba64997ec12abff8b92431b9a6eb6ddad64a78
Ayoub El Ouardi <ayoubelouardioff@gmail.com>
2025-02-15 18:37:02 +0100

    rm excutables

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,12 +8,12 @@
 void times_table(void)
 {
 	int i, j, prod;
-	
+
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{
-			prod = i * j; 
+			prod = i * j;
 			if (j == 0)
 			{
 				_putchar('0');
