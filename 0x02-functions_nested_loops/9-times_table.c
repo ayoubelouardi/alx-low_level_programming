a7bd8143b7d30c9b0e13b663603f6682469ed3ed
Ayoub El Ouardi <ayoubelouardioff@gmail.com>
2025-02-15 18:35:20 +0100

    9 is done

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * times_table - function that prints the 9 times table, starting with 0.
+ *
+ * Return: Always 0 (Success)
+ */
+void times_table(void)
+{
+	int i, j, prod;
+	
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			prod = i * j; 
+			if (j == 0)
+			{
+				_putchar('0');
+				_putchar(',');
+			}
+			else
+			{
+				if (prod < 10)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar('0' + prod);
+				}
+				else
+				{
+					_putchar(' ');
+					_putchar('0' + prod / 10);
+					_putchar('0' + prod % 10);
+
+				}
+				if (j != 9)
+					_putchar(',');
+			}
+		}
+		_putchar('\n');
+	}
+}
