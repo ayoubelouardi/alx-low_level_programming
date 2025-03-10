65442cb98f1f6a922fbff43eee082ebf3b389a89
Ayoub El Ouardi <ayoubelouardioff@gmail.com>
2025-03-10 13:05:16 +0000

    3 is solved

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,7 +3,14 @@
 
 #include <stdio.h>
 
+#define TRUE 1
+#define FALSE 0
+
+
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
+int is_leap(int year);
 
 #endif /* MAIN_H */
