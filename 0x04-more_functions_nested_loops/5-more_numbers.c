0c6561465099ea5903276156a2f1da88fa32bcf7
Ayoub El Ouardi <ayoubelouardioff@gmail.com>
2025-03-11 16:15:28 +0000

    reduce a putchar from 5 task

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,13 +10,12 @@ void more_numbers(void)
 
 	for (j = 0; j < 10; j++)
 	{
-		for (i = 0; i < 10; i++)
-			_putchar('0' + i);
-
-		for (i = 0; i < 5; i++)
+		for (i = 0; i < 15; i++)
 		{
-			_putchar('0' + 1);
-			_putchar('0' + i);
+			if (i >= 10)
+				_putchar('1');
+			_putchar('0' + (i % 10));
+
 		}
 		_putchar('\n');
 	}
